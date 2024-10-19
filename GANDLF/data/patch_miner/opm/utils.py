555f3bea5026dc662501076f87f2d20291366494
vmalefioudakis <benmalef@hotmail.com>
2024-10-19 15:54:01 +0300

    update utils.py

--- GANDLF/data/patch_miner/opm/utils.py
+++ GANDLF/data/patch_miner/opm/utils.py
@@ -509,7 +509,7 @@ def get_patch_size_in_microns(
                 metadata = input_slide.properties
                 if i == 0:
                     for _property in [
-                        tiffslide.PROPERTY_NAME_MPP_X,
+                        openslide.PROPERTY_NAME_MPP_X,
                         "tiff.XResolution",
                         "XResolution",
                     ]:
@@ -519,7 +519,7 @@ def get_patch_size_in_microns(
                             break
                 elif i == 1:
                     for _property in [
-                        tiffslide.PROPERTY_NAME_MPP_Y,
+                        openslide.PROPERTY_NAME_MPP_Y,
                         "tiff.YResolution",
                         "YResolution",
                     ]:
