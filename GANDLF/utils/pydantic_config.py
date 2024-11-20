f4d8f641548aa1d7851af9b1e1033dcfdbc844c2
vmalefioudakis <benmalef@hotmail.com>
2024-11-20 14:52:34 +0200

    add some extra parameters in pydantic_config

--- GANDLF/utils/pydantic_config.py
+++ GANDLF/utils/pydantic_config.py
@@ -64,7 +64,7 @@ class Parameters(BaseModel):
     loss_function: Union[str, Dict]
     data_augmentation: dict  # TODO: maybe is better to create a class
     nested_training: dict  # TODO: maybe is better to create a class
-    optimizer: Union[dict, str]
+    optimizer: Union[dict, str] = Field(alias="opt")
     patch_sampler: Union[dict, str]
     patch_size: Union[List[int], int]
     clip_mode: Union[str, None]
@@ -74,3 +74,4 @@ class Parameters(BaseModel):
     headers: Optional[dict] = None
     output_dir: Optional[str] = ""
     problem_type: Optional[str] = None
+    differential_privacy: Optional[dict] = {}
