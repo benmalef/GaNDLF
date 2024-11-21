59fc45dc2c0a791f16db28812026dd86b1531ae7
vmalefioudakis <benmalef@hotmail.com>
2024-11-21 10:51:30 +0200

    fix opt parameter

--- GANDLF/config_manager.py
+++ GANDLF/config_manager.py
@@ -651,7 +651,7 @@ def _parseConfig(
         parallel_compute_command = parallel_compute_command.replace('"', "")
     params["parallel_compute_command"] = parallel_compute_command
 
-    if "opt" is not None:
+    if paremeters["opt"] is not None:
         print("DeprecationWarning: 'opt' has been superseded by 'optimizer'")
         params["optimizer"] = params["opt"]
 
