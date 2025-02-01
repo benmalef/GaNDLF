5b0ae987495080b5b623adbe8a7d54887d60d221
vmalefioudakis <benmalef@hotmail.com>
2025-02-01 18:20:00 +0200

    made some code changes in generate_documentation.py

--- GANDLF/Configuration/generate_documentation.py
+++ GANDLF/Configuration/generate_documentation.py
@@ -1,6 +1,5 @@
 import utils
-import default_parameters
 
 markdown_output = utils.generate_and_save_markdown(
-    default_parameters.DefaultParameters, "doc/user_model.md"
+    default_parameters.DefaultParameters, "configuration_docs/user_model.md"
 )
