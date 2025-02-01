0e299db67a0b82f2ac4076057d8ed0d1e9b5bd0d
vmalefioudakis <benmalef@hotmail.com>
2025-02-01 18:17:53 +0200

    define user_defined_parameters.py

--- GANDLF/Configuration/user_defined_parameters.py
+++ GANDLF/Configuration/user_defined_parameters.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel, Field
-from typing import Optional, Dict
+from pydantic import BaseModel,field_validator
+from GANDLF.config_manager import  version_check
+from importlib.metadata import version
 
+#TODO: Define the paramaters from the config_manager
+
+#TODO: Some parameters maybe can define as a seperated Model
+
+class Version(BaseModel):
+    minimum: str
+    maximum: str
 
 class UserDefinedParameters(BaseModel):
-    pass
+    version: Version
+
+    @classmethod
+    @field_validator('version', mode='after')
+    def validate_version(cls, values: Version) -> Version:
+        if version_check(values.model_dump(), version_to_check=version("GANDLF")):
+            return values
