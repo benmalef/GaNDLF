39c5b1bee01f5498c7c851a73eff12ecc6f1cab3
vmalefioudakis <benmalef@hotmail.com>
2025-02-01 18:22:23 +0200

    add a temp test dir for testing purposes

---
+++ test_configuration/test_configuration.py
@@ -0,0 +1,7 @@
+from GANDLF.config_manager import ConfigManager
+from pathlib import Path
+if __name__ == "__main__":
+    testingDir = Path(__file__).parent.absolute().__str__()
+    parameters = ConfigManager(testingDir+"/config_all_options.yaml",version_check_flag=False)
+    print(parameters)
+
