576577d6508c488e77ed023010206b13bce4535d
vmalefioudakis <benmalef@hotmail.com>
2025-02-01 18:19:23 +0200

    create parameters(BaseModel)

---
+++ GANDLF/Configuration/parameters.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, ConfigDict
+from GANDLF.Configuration.default_parameters import DefaultParameters
+from GANDLF.Configuration.user_defined_parameters import UserDefinedParameters
+
+class ParametersConfiguration(BaseModel):
+    model_config = ConfigDict()
+
+class Parameters(ParametersConfiguration,DefaultParameters,UserDefinedParameters):
+    pass
