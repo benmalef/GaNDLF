2ddad48b807021386a367a662a2a3c86349c39c0
vmalefioudakis <benmalef@hotmail.com>
2025-02-03 21:47:17 +0200

    refactor user_defined_parameters.py

--- GANDLF/Configuration/user_defined_parameters.py
+++ GANDLF/Configuration/user_defined_parameters.py
@@ -1,18 +1,33 @@
-from pydantic import BaseModel, field_validator
+from typing import Union
+from pydantic import BaseModel, model_validator, Field
 from GANDLF.config_manager import version_check
 from importlib.metadata import version
+from typing_extensions import Self
 
 
 class Version(BaseModel):
     minimum: str
     maximum: str
 
+    @model_validator(mode="after")
+    def validate_version(self) -> Self:
+        if version_check(self.model_dump(), version_to_check=version("GANDLF")):
+            return self
+
+
+class PatchSize(BaseModel):
+    patch_size: list[Union[int, float]]
+
+    @model_validator(mode="after")
+    def validate_patch_size(self) -> Self:
+        if isinstance(self.patch_size, int) or isinstance(self.patch_size, float):
+            self.patch_size = [self.patch_size]
+            return self
+
 
 class UserDefinedParameters(BaseModel):
-    version: Version
-
-    @classmethod
-    @field_validator("version", mode="after")
-    def validate_version(cls, values: Version) -> Version:
-        if version_check(values.model_dump(), version_to_check=version("GANDLF")):
-            return values
+    version: Version = Field(
+        default=Version(minimum=version("GANDLF"), maximum=version("GANDLF")),
+        description="Whether weighted loss is to be used or not.",
+    )
+    patch_size: PatchSize = Field(..., description="Patch size.")
