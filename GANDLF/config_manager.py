2cf78f31f778460594e7523ae3c186837337119f
vmalefioudakis <benmalef@hotmail.com>
2025-02-09 12:46:45 +0200

    black

--- GANDLF/config_manager.py
+++ GANDLF/config_manager.py
@@ -1,7 +1,7 @@
 # import logging
 from typing import Optional, Union
 from pydantic import ValidationError
-import  yaml
+import yaml
 
 
 from GANDLF.Configuration.Parameters.parameters import Parameters
