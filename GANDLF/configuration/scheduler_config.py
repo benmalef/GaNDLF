8b36c3716124fb895d1057f73f658ff73d96045a
vmalefioudakis <benmalef@hotmail.com>
2025-03-22 14:05:50 +0200

    refactor

--- GANDLF/configuration/scheduler_config.py
+++ GANDLF/configuration/scheduler_config.py
@@ -5,13 +5,17 @@ from GANDLF.schedulers import global_schedulers_dict
 
 TYPE_OPTIONS = Literal[tuple(global_schedulers_dict.keys())]
 
+class base_triangle_config(BaseModel):
+    min_lr: float = Field(default= (10 ** -3))
+    max_lr: float = Field(default=1)
+
+
 
 # It allows extra parameters
 class SchedulerConfig(BaseModel):
     model_config = ConfigDict(extra="allow")
     type: TYPE_OPTIONS = Field(
-        description="triangle/triangle_modified use LambdaLR but triangular/triangular2/exp_range uses CyclicLR"
+        description="triangle/triangle_modified use LambdaLR but triangular/triangular2/exp_range uses CyclicLR",
+        default = "triangle"
     )
-    # min_lr: 0.00001, #TODO: this should be defined ??
-    # max_lr: 1, #TODO: this should be defined ??
     step_size: float = Field(description="step_size", default=None)
